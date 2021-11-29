92bbe844e505df71539626f8b3211590984951d0
Simon Franzen <simon@zauberware.com>
2021-11-29 17:45:16 +0100

    update config

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -80,8 +80,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('yucca_prerender');
+        $treeBuilder = new TreeBuilder('yucca_prerender');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
